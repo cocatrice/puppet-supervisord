3fe46bb12cb306e9c5e87bcfcd39f81a6cefabd1
Thai Pham <tpham@freelancer.com>
2019-08-06 09:35:24 +1000

    Ensure latest version of `setuptools`

--- manifests/pip.pp
+++ manifests/pip.pp
@@ -5,30 +5,38 @@
 class supervisord::pip inherits supervisord {
 
   Exec {
-    path => '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin'
+    path => '/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
   }
 
-  # removing due to duplicate declaration FLN codebase
-  # ensure_packages('curl')
+  if ! defined(Package['curl']) {
+    ensure_packages('curl')
+  }
 
   exec { 'install_setuptools':
     command => "curl ${supervisord::setuptools_url} | python",
     cwd     => '/tmp',
     unless  => 'which easy_install',
     before  => Exec['install_pip'],
-    require => Package['curl']
+    require => Package['curl'],
   }
 
   exec { 'install_pip':
     command => 'easy_install pip',
-    unless  => 'which pip'
+    unless  => 'which pip',
+  }
+
+  # See https://github.com/pypa/setuptools/issues/581
+  exec { 'reinstall_setuptools':
+    command => 'pip install --upgrade setuptools',
+    onlyif  => 'pip list | grep "setuptools 33.1.1"',
+    require => Exec['install_pip'],
   }
 
   if $::osfamily == 'RedHat' {
     exec { 'pip_provider_name_fix':
       command   => 'alternatives --install /usr/bin/pip-python pip-python /usr/bin/pip 1',
       subscribe => Exec['install_pip'],
-      unless    => 'which pip-python'
+      unless    => 'which pip-python',
     }
   }
 
