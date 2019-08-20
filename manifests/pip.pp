30fc61d3da7181ec85209e1470acf6870e6521c4
Thai Pham <tpham@freelancer.com>
2019-08-20 18:09:28 +1000

    Use default language when reinstalling `setuptools`

--- manifests/pip.pp
+++ manifests/pip.pp
@@ -15,21 +15,28 @@ class supervisord::pip inherits supervisord {
   exec { 'install_setuptools':
     command => "curl ${supervisord::setuptools_url} | python",
     cwd     => '/tmp',
-    unless  => 'which easy_install',
-    before  => Exec['install_pip'],
     require => Package['curl'],
+    unless  => 'which easy_install',
   }
 
   exec { 'install_pip':
     command => 'easy_install pip',
+    require => Exec['install_setuptools'],
     unless  => 'which pip',
   }
 
   # See https://github.com/pypa/setuptools/issues/581
-  exec { 'reinstall_setuptools':
-    command => 'pip install --upgrade setuptools',
-    onlyif  => 'pip list | grep "setuptools 33.1.1" || ! pip list | grep "setuptools"',
+  exec { 'upgrade_pip':
+    command => 'pip install --upgrade pip',
+    onlyif  => 'pip list 2>&1 | grep "You should consider upgrading"',
     require => Exec['install_pip'],
+  }
+
+  exec { 'upgrade_setuptools':
+    command     => 'pip install --upgrade setuptools || pip list | grep "setuptools"',
+    environment => ['LC_ALL=C'],
+    onlyif      => 'pip list | grep "setuptools" | grep "33.1.1" || ! pip list | grep "setuptools"',
+    require     => Exec['upgrade_pip'],
   }
 
   if $::osfamily == 'RedHat' {
